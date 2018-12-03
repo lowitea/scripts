0b2a3bf17ccfb344f3ed24a70ada4f085d3103ba
Evgeniy Mitin <ea@lowit.ru>
2018-12-03 20:18:41 +0300

    +

--- git-clean.sh
+++ git-clean.sh
@@ -1,2 +1,3 @@
-git branch --merged | grep -v '*' | grep -v 'develop' | xargs git branch -D  && git reset --hard && git clean -d -x -f
+#!/bin/bash
 
+git branch --merged | grep -v '*' | grep -v 'develop' | xargs git branch -D  && git reset --hard && git clean -d -x -f
