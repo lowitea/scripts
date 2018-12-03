234b92b8b4b88f9a3d661a9a82961ce268ba26b9
Evgeniy Mitin <ea@lowit.ru>
2018-12-03 20:16:20 +0300

    Initial commit

---
+++ git-clean.sh
@@ -0,0 +1,2 @@
+git branch --merged | grep -v '*' | grep -v 'develop' | xargs git branch -D  && git reset --hard && git clean -d -x -f
+
---
+++ resp_time.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+if [[ -z $1 ]]; then
+    echo "url not found"
+    exit
+fi
+
+if [[ -z $2 ]]; then
+    resp_count=20
+else
+    resp_count=$2
+fi
+
+while [  $resp_count -ne 0 ]; do
+    curl -Is -w "%{http_code} Time: %{time_total}\n" $1 | grep 'Time' | tr , .
+    let resp_count=resp_count-1 
+done | awk '{print "\033[0;31mCode:\033[0m", $1, "\033[0;31mTime:\033[0m", $3} {sum+=$3} END {print "Average = ",sum/NR}'
