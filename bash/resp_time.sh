ee88b3091539045e9441bd29b262247484a83e90
lowit <ea@lowit.ru>
2019-03-21 12:43:10 +0300

    fixed print status

--- bash/resp_time.sh
+++ bash/resp_time.sh
@@ -15,7 +15,7 @@ else
 fi
 
 while [  $resp_count -ne 0 ]; do
-    curl -Is -w "%{http_code} Time: %{time_total}\n" $1 | grep 'Time' | tr , .
+    curl -Is -w "%{http_code} Time: %{time_total}\n" $1 | grep ' Time' | tr , .
     let resp_count=resp_count-1 
 done | awk '{print "\033[0;31mCode:\033[0m", $1, "\033[0;31mTime:\033[0m", $3} {sum+=$3} END {print "Average = ",sum/NR}'
 
