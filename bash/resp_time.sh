8040e550001f1615daeed1c83bfd646256f995a8
lowit <ea@lowit.ru>
2019-03-21 12:36:36 +0300

    fixed exit form script

--- bash/resp_time.sh
+++ bash/resp_time.sh
@@ -5,7 +5,7 @@
 
 if [[ -z $1 ]]; then
     echo "url not found"
-    exit
+    return
 fi
 
 if [[ -z $2 ]]; then
@@ -18,3 +18,4 @@ while [  $resp_count -ne 0 ]; do
     curl -Is -w "%{http_code} Time: %{time_total}\n" $1 | grep 'Time' | tr , .
     let resp_count=resp_count-1 
 done | awk '{print "\033[0;31mCode:\033[0m", $1, "\033[0;31mTime:\033[0m", $3} {sum+=$3} END {print "Average = ",sum/NR}'
+
