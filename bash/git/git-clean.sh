f8f882d909af9947b317089dd538f88d86c4a071
lowit <ea@lowit.ru>
2019-09-04 11:42:32 +0300

    fixes docstring error

--- bash/git/git-clean.sh
+++ bash/git/git-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Git. Remove merget branches.
+# Git. Remove merged branches.
 
 git branch --merged | grep -v '*' | grep -v 'develop' | xargs git branch -D
 git branch -vv | grep '\[origin/.*: gone\]' | grep -v '*' | grep -v 'develop' | awk '{print $1}' | xargs git branch -D
