1ff23b5e09adda40014aecbbb7e398cd0651d36e
lowit <ea@lowit.ru>
2020-02-05 15:15:11 +0300

    update git clean

--- bash/git/git-clean.sh
+++ bash/git/git-clean.sh
@@ -2,5 +2,5 @@
 # Git. Remove merged branches.
 
 git branch --merged | grep -v '*' | grep -v 'develop' | xargs git branch -D
-git branch -vv | grep '\[origin/.*: gone\]' | grep -v '*' | grep -v 'develop' | awk '{print $1}' | xargs git branch -D
+git branch -vv | grep '\[origin/.*: gone\]' | grep -v '*' | awk '{print $1}' | xargs git branch -D
 
