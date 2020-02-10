805ba6bd834e048792f7f778adc28652c13fb76b
lowit <ea@lowit.ru>
2020-02-10 12:18:57 +0300

    master: update downloads cleaner

--- bash/downloads_cleaner.sh
+++ bash/downloads_cleaner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # clean Downloads directory
-# delete all files older 15 days
+# delete all files older 7 days
 # for crontab:
 # 0 */4 * * * ~/scripts/bash/downloads_cleaner.sh
 
-find ~/Downloads -mtime +15 -delete
+find ~/Downloads -mtime +7 -delete
 
